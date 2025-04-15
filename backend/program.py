84691d1c13c09552556b3940b0014b540f56183c
David Beneš <david@davidbenes.cz>
2025-04-15 19:31:34 +0200

    corrected the location of index.html

--- backend/program.py
+++ backend/program.py
@@ -1,18 +1,23 @@
 import webbrowser
 import http.server
 import socketserver
+import os
 
 PORT = 8000
 
-# Define handler
+# Get absolute path to frontend folder relative to this script
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FRONTEND_DIR = os.path.join(BASE_DIR, "../frontend")
+
+# Change working directory to frontend
+os.chdir(os.path.abspath(FRONTEND_DIR))
+
 Handler = http.server.SimpleHTTPRequestHandler
 
-def open():
-    # Opens the UI in a web browser.
+def run():
     webbrowser.open(f"http://localhost:{PORT}/index.html")
 
-# Start the server
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    open()
+    run() # Only for testing purposes, comment out while running the server.
     print(f"Server running at http://localhost:{PORT}")
     httpd.serve_forever()
